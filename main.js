b51d12919803ad246c19f327ae6cbcc639cba1a5
Dawid Zoń <dawid.zon12@gmail.com>
2022-11-20 08:27:02 +0100

    add main.js

---
+++ main.js
@@ -0,0 +1,146 @@
+const articles = [
+  {
+    id: 1,
+    title: "Umar na śmierć",
+    body: "Umar na śmierć poniewaz udławiił się mydłem",
+    likesCount: 0,
+  },
+  {
+    id: 2,
+    title: "2",
+    body: "dwa",
+    likesCount: 0,
+  },
+  {
+    id: 3,
+    title: "3",
+    body: "trzy",
+    likesCount: 0,
+  },
+  {
+    id: 4,
+    title: "4",
+    body: "cztery",
+    likesCount: 0,
+  },
+  {
+    id: 5,
+    title: "5",
+    body: "piec",
+    likesCount: 0,
+  },
+];
+
+function render() {
+  const rootElement = document.querySelector("#root");
+  rootElement.innerHTML = "";
+
+  const numberOfPosts = document.createElement("div");
+  numberOfPosts.innerText =
+    "Ilość postów na stronie wynosi: " + articles.length;
+
+  const addArticleTitle = document.createElement("input");
+  addArticleTitle.setAttribute("type", "text");
+  addArticleTitle.setAttribute("id", "articleTitle-input");
+
+  const addArticleBody = document.createElement("input");
+  addArticleBody.setAttribute("type", "text");
+  addArticleBody.setAttribute("id", "articleBody-input");
+
+  const addArticleButton = document.createElement("input");
+  addArticleButton.setAttribute("type", "button");
+  addArticleButton.setAttribute("value", "Dodaj");
+  addArticleButton.setAttribute("id", "delete-article");
+
+  addArticleButton.onclick = () => {
+    const newArticleTitle = addArticleTitle.value;
+    const newArticleBody = addArticleBody.value;
+    articles.push({
+      id: articles.length + 1,
+      title: newArticleTitle,
+      body: newArticleBody,
+      likesCount: 0,
+    });
+    render();
+  };
+
+  const tableElement = document.createElement("table");
+  tableElement.setAttribute("class", "table table-bordered table-primary");
+  const tbElement = document.createElement("tbody");
+  const trElement = document.createElement("tr");
+  const th1Element = document.createElement("th");
+  const th2Element = document.createElement("th");
+  const th3Element = document.createElement("th");
+  const th4Element = document.createElement("th");
+  th1Element.textContent = "id";
+  th2Element.textContent = "title";
+  th3Element.textContent = "body";
+  th4Element.textContent = "likesCount";
+  tbElement.appendChild(trElement);
+  trElement.appendChild(th1Element);
+  trElement.appendChild(th2Element);
+  trElement.appendChild(th3Element);
+  trElement.appendChild(th4Element);
+  tableElement.appendChild(trElement);
+
+  articles.forEach((article, index) => {
+    const tbArticleElement = document.createElement("tbody");
+    const trArticleElement = document.createElement("tr");
+    const tdIdArticleElement = document.createElement("td");
+    const tdTitleArticleElement = document.createElement("td");
+    const tdBodyArticleElement = document.createElement("td");
+    const tdLikesCountArticleElement = document.createElement("td");
+
+    const tdDeleteElement = document.createElement("input");
+    tdDeleteElement.setAttribute("type", "button");
+    tdDeleteElement.setAttribute("value", "Usun");
+    tdDeleteElement.setAttribute("id", "delete");
+
+    tdDeleteElement.onclick = () => {
+      articles.splice(index, 1);
+      render();
+    };
+
+    tdIdArticleElement.textContent = article.id;
+    tdTitleArticleElement.textContent = article.title;
+    tdBodyArticleElement.textContent = article.body;
+    tdLikesCountArticleElement.textContent = article.likesCount;
+
+    const tdPlusLikesCountArticleElement = document.createElement("input");
+    tdPlusLikesCountArticleElement.setAttribute("type", "button");
+    tdPlusLikesCountArticleElement.setAttribute("value", "Plus");
+    const tdMinusLikesCountArticleElement = document.createElement("input");
+    tdMinusLikesCountArticleElement.setAttribute("type", "button");
+    tdMinusLikesCountArticleElement.setAttribute("value", "Minus");
+
+    tdPlusLikesCountArticleElement.onclick = () => {
+      article.likesCount++;
+      render();
+      alert("Zalajkowales");
+    };
+
+    tdMinusLikesCountArticleElement.onclick = () => {
+      article.likesCount--;
+      render();
+      alert("Odlajkowales");
+    };
+
+    trArticleElement.appendChild(tdIdArticleElement);
+    trArticleElement.appendChild(tdTitleArticleElement);
+    trArticleElement.appendChild(tdBodyArticleElement);
+    trArticleElement.appendChild(tdLikesCountArticleElement);
+    trArticleElement.appendChild(tdPlusLikesCountArticleElement);
+    trArticleElement.appendChild(tdMinusLikesCountArticleElement);
+    trArticleElement.appendChild(tdDeleteElement);
+    tbArticleElement.appendChild(trArticleElement);
+    tableElement.appendChild(tbArticleElement);
+  });
+
+  rootElement.appendChild(addArticleTitle);
+  rootElement.appendChild(addArticleBody);
+  rootElement.appendChild(addArticleButton);
+  rootElement.appendChild(numberOfPosts);
+  rootElement.appendChild(tableElement);
+}
+
+render();
